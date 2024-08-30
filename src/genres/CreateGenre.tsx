5ac74ca3cf9534130f0615868def1a0a29d180ce
pranayskumbhar <pranayskumbhar@gmail.com>
2024-08-30 09:09:54 +0530

    facing formik for erroe in console

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,15 +1,37 @@
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../utils/Button";
+import { Field, Form, Formik } from "formik";
 
 export default function CreateGenre() {
-  const history = useHistory();
+  // const history = useHistory();
   return (
     <>
       <h3>Create genre</h3>
-      <Button onClick={() => {
+
+      <Formik
+        initialValues={{
+          name: "",
+        }}
+        onSubmit={(value) => {
+          //when the form is posted
+          console.log(value);
+        }}
+      >
+        <Form placeholder="" onPointerEnterCapture="" onPointerLeaveCapture="">
+          <div className="mb-3">
+            <label htmlFor="name">Name</label>
+            <Field className="form-control" name="name" />
+          </div>
+          <Button type="submit">Save Changes</Button>
+          <Link className="btn btn-secondary" to="/genres">
+            Cancel
+          </Link>
+        </Form>
+      </Formik>
+      {/* <Button onClick={() => {
         //saving data to db
         history.push("/genres");
-      }}>Save Genre</Button>
+      }}>Save Genre</Button> */}
     </>
   );
 }
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -1,12 +1,18 @@
 import { ReactElement, ReactNode } from "react";
 
 export default function Button(props: buttonProps) {
-  return <button className="btn btn-primary"
+  return <button type={props.type} className="btn btn-primary"
   onClick={props.onClick}
   >{props.children}</button>
 }
 
 interface buttonProps {
   children: ReactNode;
-  onClick() : void;
+  onClick?() : void;
+  type : "button" | "submit";
+ }
+
+
+Button.defaultProps = {
+  type : "button"
 }
