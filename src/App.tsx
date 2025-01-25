cb6ca3ee3a16a4a571dd7ee220e069d35e99ed9b
pranayskumbhar <pranayskumbhar@gmail.com>
2025-01-25 20:27:39 +0530

    97 complete footer added

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import IndexGenres from "./genres/IndexGenres";
 import LandingPage from "./movies/LandingPage";
 import routes from "./route-config";
-import configureValidations from "./Validations"
+import configureValidations from "./Validations";
 
 configureValidations();
-
 
 function App() {
   return (
@@ -23,14 +22,18 @@ function App() {
         {/* <Button>Whatever Text</Button> */}
         {/* <IndividualMovie {...testMovie} /> */}
         <Switch>
-          {
-          routes.map((route) => (
+          {routes.map((route) => (
             <Route key={route.path} path={route.path} exact={route.exact}>
-                < route.component />
+              <route.component />
             </Route>
-          ))
-          }
+          ))}
         </Switch>
+        <footer className="bd-footer py-5 mt-5 bg-loight">
+          <div className="container">
+            &copy; - React Movies {new Date().getFullYear().toString()} - Made
+            With Love ❤
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
