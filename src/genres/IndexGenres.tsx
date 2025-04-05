13364e10f96594232f5951c20ccbc4fb6676354e
pranayskumbhar <pranayskumbhar@gmail.com>
2025-04-05 21:59:51 +0530

    119 complete

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios, { AxiosResponse } from "axios";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { genreDTO } from "./genres.model";
+import { urlGenres } from "../endpoints";
+
 export default function IndexGenres() {
-   return (
+  useEffect(() => {
+    console.log(urlGenres);
+
+    axios
+      .get(urlGenres)
+      .then((response: AxiosResponse<genreDTO[]>) => {
+        // .then((response) => {
+        console.log(response.data);
+      })
+      .catch((exception) => {
+        console.log(exception);
+      });
+  }, []);
+
+  return (
     <>
       <h3>Genres List </h3>
-      <Link className="btn btn-primary" to="/genres/create">Create Genre</Link>
+      <Link className="btn btn-primary" to="/genres/create">
+        Create Genre
+      </Link>
     </>
   );
 }
